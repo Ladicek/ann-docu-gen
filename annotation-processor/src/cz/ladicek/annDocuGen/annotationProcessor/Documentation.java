a6d812b8aa403a42004c97944e98e527f5d257d4
Ladislav Thon <ladicek@gmail.com>
2014-09-09 21:05:30 +0200

    handle Optional types properly
    Proper handling of Optionals means:
    
    1. Deciding if something is Optional is not based on comparing strings
       but actual types.
    2. If a field is Optional and has no initializer, it is implied to be
       initialized to Optional.absent

--- annotation-processor/src/cz/ladicek/annDocuGen/annotationProcessor/Documentation.java
+++ annotation-processor/src/cz/ladicek/annDocuGen/annotationProcessor/Documentation.java
@@ -4,17 +4,23 @@ import cz.ladicek.annDocuGen.api.Property;
 
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.Element;
+import javax.lang.model.type.TypeMirror;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 public final class Documentation {
+    private static final String OPTIONAL_TYPE_FQN = "cz.ladicek.annDocuGen.api.Optional";
+
     private final ProcessingEnvironment processingEnv;
+    private final TypeMirror optionalTypeErased;
     private final FieldInitializerDiscovery fieldInitializerDiscovery;
     private final Map<String, DocumentedClass> classes = new HashMap<String, DocumentedClass>();
 
     public Documentation(ProcessingEnvironment processingEnv) {
         this.processingEnv = processingEnv;
+        this.optionalTypeErased = processingEnv.getTypeUtils().erasure(
+                processingEnv.getElementUtils().getTypeElement(OPTIONAL_TYPE_FQN).asType());
         this.fieldInitializerDiscovery = FieldInitializerDiscovery.Factory.create(processingEnv);
     }
 
@@ -35,10 +41,16 @@ public final class Documentation {
     }
 
     public DocumentedProperty documentPropertyField(Element annotatedField) {
+        TypeMirror fieldTypeErased = processingEnv.getTypeUtils().erasure(annotatedField.asType());
+        boolean isOptional = processingEnv.getTypeUtils().isSameType(optionalTypeErased, fieldTypeErased);
+
         String name = annotatedField.getAnnotation(Property.class).value();
         String type = annotatedField.asType().toString();
         String initializer = fieldInitializerDiscovery.getFor(annotatedField);
-        boolean mandatory = initializer == null && !type.contains("Optional");
+        if (initializer == null && isOptional) {
+            initializer = "Optional.absent() /* implied */";
+        }
+        boolean mandatory = initializer == null;
         String javadoc = processingEnv.getElementUtils().getDocComment(annotatedField);
         return new DocumentedProperty(name, type, initializer, mandatory, javadoc);
     }
