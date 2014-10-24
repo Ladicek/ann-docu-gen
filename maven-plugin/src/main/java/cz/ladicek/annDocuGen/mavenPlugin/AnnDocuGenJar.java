c1d0226597e7bb94d18608c60a29dff6ec08113b
Ladislav Thon <ladicek@gmail.com>
2014-10-24 22:43:45 +0200

    add a Maven plugin
    The AnnDocuGen annotation processor generates documentation, but the generated
    documentation is just a simple directory and Maven can't work with it. That
    is a problem in the real-world usecase. Ideally, it should work similarly to
    how javadoc works in Maven. Luckily, that's pretty easy to achieve.
    
    This adds a simple Maven plugin that creates a JAR archive of the generated
    documentation and attaches it to the Maven project as an artifact. This means
    that the generated documentation will be deployed to a Maven repository
    automatically. The artifact has a classifier of "anndocugen".
    
    To show how the plugin is used, it is applied to the example module.

---
+++ maven-plugin/src/main/java/cz/ladicek/annDocuGen/mavenPlugin/AnnDocuGenJar.java
@@ -0,0 +1,143 @@
+package cz.ladicek.annDocuGen.mavenPlugin;
+
+import org.apache.maven.archiver.MavenArchiveConfiguration;
+import org.apache.maven.archiver.MavenArchiver;
+import org.apache.maven.artifact.DependencyResolutionRequiredException;
+import org.apache.maven.artifact.handler.ArtifactHandler;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectHelper;
+import org.codehaus.plexus.archiver.Archiver;
+import org.codehaus.plexus.archiver.ArchiverException;
+import org.codehaus.plexus.archiver.jar.JarArchiver;
+import org.codehaus.plexus.archiver.jar.ManifestException;
+
+import java.io.File;
+import java.io.IOException;
+
+@Mojo(name = "jar", defaultPhase = LifecyclePhase.PACKAGE, requiresDependencyResolution = ResolutionScope.COMPILE)
+public class AnnDocuGenJar extends AbstractMojo {
+    @Component
+    private MavenProjectHelper projectHelper;
+
+    @Component(role = Archiver.class, hint = "jar")
+    private JarArchiver jarArchiver;
+
+    @Parameter
+    private MavenArchiveConfiguration archive = new MavenArchiveConfiguration();
+
+    @Parameter(defaultValue = "${session}", required = true, readonly = true)
+    private MavenSession session;
+
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    private MavenProject project;
+
+    @Parameter(property = "project.build.directory", required = true, readonly = true)
+    private String outputDirectory;
+
+    @Parameter(property = "project.build.finalName", required = true, readonly = true)
+    private String finalName;
+
+    /** Directory from where AnnDocuGen-generated documentation should be archived. */
+    @Parameter(property = "annDocuGen.directory",
+            defaultValue = "${project.build.directory}/generated-sources/annotations/annDocuGen", required = true)
+    private File annDocuGenDirectory;
+
+    /** Whether to skip this goal. */
+    @Parameter(property = "annDocuGen.skip", defaultValue = "false")
+    private boolean skip;
+
+    /** Whether to attach the generated file to project's artifacts. */
+    @Parameter(property = "annDocuGen.attach", defaultValue = "true")
+    private boolean attach;
+
+    /** The classifier of the generated artifact. */
+    @Parameter(property = "annDocuGen.classifier", defaultValue = "anndocugen", required = true)
+    private String classifier;
+
+    /** Whether to fail the build if an error happens. */
+    @Parameter(property = "annDocuGen.failOnError", defaultValue = "true")
+    private boolean failOnError;
+
+    public void execute() throws MojoExecutionException {
+        if (skip) {
+            getLog().info("Skipping AnnDocuGen archivation");
+            return;
+        }
+
+        if (!"pom".equalsIgnoreCase(project.getPackaging())) {
+            ArtifactHandler artifactHandler = project.getArtifact().getArtifactHandler();
+            if (!"java".equals(artifactHandler.getLanguage())) {
+                getLog().info("Not archiving AnnDocuGen as the project is not a Java classpath-capable package");
+                return;
+            }
+        }
+
+        try {
+            File outputFile = generateArchive(annDocuGenDirectory, finalName + "-" + classifier + ".jar");
+
+            if (!attach) {
+                getLog().info("NOT adding AnnDocuGen JAR to attached artifacts list");
+            } else {
+                projectHelper.attachArtifact(project, outputFile, classifier);
+            }
+        } catch (ArchiverException e) {
+            failOnError("ArchiverException: Error while creating archive", e);
+        } catch (IOException e) {
+            failOnError("IOException: Error while creating archive", e);
+        } catch (RuntimeException e) {
+            failOnError("RuntimeException: Error while creating archive", e);
+        }
+    }
+
+    private File generateArchive(File annDocuGenDirectory, String jarFileName) throws ArchiverException, IOException {
+        File annDocuGenJar = new File(outputDirectory, jarFileName);
+
+        if (annDocuGenJar.exists()) {
+            annDocuGenJar.delete();
+        }
+
+        MavenArchiver archiver = new MavenArchiver();
+        archiver.setArchiver(jarArchiver);
+        archiver.setOutputFile(annDocuGenJar);
+
+        if (!directoryContainsAnnDocuGenOutput(annDocuGenDirectory)) {
+            getLog().warn("JAR will be empty, no AnnDocuGen found in " + annDocuGenDirectory);
+        } else {
+            archiver.getArchiver().addDirectory(annDocuGenDirectory);
+        }
+
+        try {
+            archive.setAddMavenDescriptor(false);
+            archiver.createArchive(session, project, archive);
+        } catch (ManifestException e) {
+            throw new ArchiverException("ManifestException: " + e.getMessage(), e);
+        } catch (DependencyResolutionRequiredException e) {
+            throw new ArchiverException("DependencyResolutionRequiredException: " + e.getMessage(), e);
+        }
+
+        return annDocuGenJar;
+    }
+
+    private boolean directoryContainsAnnDocuGenOutput(File dir) {
+        return dir.exists() && new File(dir, "index.html").exists();
+    }
+
+    private void failOnError(String prefix, Exception e) throws MojoExecutionException {
+        if (failOnError) {
+            if (e instanceof RuntimeException) {
+                throw (RuntimeException) e;
+            }
+            throw new MojoExecutionException(prefix + ": " + e.getMessage(), e);
+        }
+
+        getLog().error(prefix + ": " + e.getMessage(), e);
+    }
+}
